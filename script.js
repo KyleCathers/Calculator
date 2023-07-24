8de67aad14f48169ff1d2dd2ec68b4a20ceeb02b
Kyle Cathers <kyle_c@live.ca>
2023-07-23 22:36:48 -0700

    Commit mostly functional Clear, backspace, +, -, *, /, = and 0-9 and decimal operators working need to continue searching for bugs/edge cases. Add functionality for plus minus (sign switch) button. Also want to add functionality to use result of operation (state 4) and continue into further calculations. For example: 3x4=12, then using the 12 for 12/3=4 etc... Currently need to clear after the first calculation is complete!

--- script.js
+++ script.js
@@ -1,31 +1,230 @@
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
-let percentage = a => a / 100;
-let exponent = (a, b) => a ^ b;
+let firstOperand = '';
+let operator = '';
+let secondOperand = '';
+let result = 0;
+let state = 0;
+let operandHasDecimal = 0;
 
-let firstOperand;
-let operator;
-let secondOperand;
+let screenBottomRow = document.querySelector('.bottomRow');
+let screenTopRow = document.querySelector('.topRow');
 
-function operate(firstOperand, secondOperand, operator) {
-    if (operator == 'add') {
-        return add(a, b);
-    } else if (operator == 'subtract') {
-        return multiply(a, b);
-    } else if (operator == 'multiply') {
-        return multiply(a, b);
-    } else if (operator == 'divide') {
-        return divide(a, b);
-    } else if (operator == 'percentage') {
-        return percentage(a);
-    } else if (operator == 'exponent') {
-        return exponent(a, b);
+let oneButton = document.querySelector('#one');
+let twoButton = document.querySelector('#two');
+let threeButton = document.querySelector('#three');
+let fourButton = document.querySelector('#four');
+let fiveButton = document.querySelector('#five');
+let sixButton = document.querySelector('#six');
+let sevenButton = document.querySelector('#seven');
+let eightButton = document.querySelector('#eight');
+let nineButton = document.querySelector('#nine');
+let zeroButton = document.querySelector('#zero');
+let decimalButton = document.querySelector('#decimal');
+
+let addButton = document.querySelector('#add');
+let subtractButton = document.querySelector('#subtract');
+let multiplyButton = document.querySelector('#multiply');
+let divideButton = document.querySelector('#divide');
+let equalsButton = document.querySelector('#equals');
+let plusMinusButton = document.querySelector('#plusMinus');
+
+let clearButton = document.querySelector('#clear');
+let backspaceButton = document.querySelector('#backspace');
+
+clearButton.addEventListener('click', () => {
+    state = 0;
+    buttonPressed();
+});
+
+zeroButton.addEventListener('click', () => { 
+    numberPressed('0');
+});
+
+oneButton.addEventListener('click', () => {
+    numberPressed('1');
+});
+
+twoButton.addEventListener('click', () => {
+    numberPressed('2');
+});
+
+threeButton.addEventListener('click', () => {
+    numberPressed('3');
+});
+
+fourButton.addEventListener('click', () => {
+    numberPressed('4');
+});
+
+fiveButton.addEventListener('click', () => {
+    numberPressed('5');
+});
+
+sixButton.addEventListener('click', () => {
+    numberPressed('6');
+});
+
+sevenButton.addEventListener('click', () => {
+    numberPressed('7');
+});
+
+eightButton.addEventListener('click', () => {
+    numberPressed('8');
+});
+
+nineButton.addEventListener('click', () => {
+    numberPressed('9');
+});
+
+decimalButton.addEventListener('click', () => {
+    if (operandHasDecimal == 0){ // only 1 decimal per operand
+        operandHasDecimal = 1;
+        numberPressed('.');
+    }
+});
+
+addButton.addEventListener('click', () => {
+    operatorPressed('+');
+})
+
+subtractButton.addEventListener('click', () => {
+    operatorPressed('-');
+})
+
+multiplyButton.addEventListener('click', () => {
+    operatorPressed('x');
+})
+
+divideButton.addEventListener('click', () => {
+    operatorPressed('/');
+})
+
+equalsButton.addEventListener('click', () => {
+    equalsPressed();
+})
+
+backspaceButton.addEventListener('click', () => {
+    if (state == 1) {
+        firstOperand = firstOperand.slice(0, -1);
+        buttonPressed();
+    } else if (state == 3) {
+        secondOperand = secondOperand.slice(0, -1);
+        buttonPressed();
+    } else if (state == 4) {
+        //console.log(`state: ${state}, operand1: ${firstOperand}, operator: ${operator}, operand2: ${secondOperand}`);
+    }
+})
+
+function operate(a, b, operator) {
+    if (operator == '+') {
+        return a + b;
+    } else if (operator == '-') {
+        return a - b;
+    } else if (operator == 'x') {
+        return a * b;
+    } else if (operator == '/') {
+        return a/b;
     } else {
         return null;
     }
 }
 
-let bottomRow = document.querySelector('.bottomRow');
-bottomRow.textContent = '1';
+function operatorPressed(operatorInput) {
+    if (state == 1) {
+        state = 2;
+        operator = operatorInput;
+        buttonPressed();
+    } else if (state == 2) {
+        operator = operatorInput;
+        buttonPressed();
+    } else if (state == 4) {
+
+    } else if (state == 'Overflow') {
+        buttonPressed();
+    }
+}
+
+function equalsPressed() {
+    if (state == 3) {
+        state = 4;
+        result = formatNumber(operate(Number(firstOperand), Number(secondOperand), operator).toString());
+        buttonPressed();
+    } else if (state == 'Overflow') {
+        buttonPressed();
+    }
+}
+
+function numberPressed(number) {
+    if (state == 0) {
+        state = 1;
+        firstOperand += number;
+        buttonPressed();
+    } else if (state == 1) {
+        if (firstOperand.length < 9) {
+            firstOperand += number;
+        }
+        buttonPressed();
+    } else if (state == 2) {
+        state = 3;
+        secondOperand += number;
+        buttonPressed();
+    } else if (state == 3) {
+        if ((secondOperand.length < 9) & ((firstOperand + operator + secondOperand).length < 14)) {
+            secondOperand += number;
+        }
+        buttonPressed();
+    } else if (state == 4) {
+        buttonPressed();
+    } else if (state == 'Overflow') {
+        buttonPressed();
+    }
+}
+
+function clearState() {
+    firstOperand = '';
+    operator = '';
+    secondOperand = '';
+    operandHasDecimal = 0;
+    screenTopRow.innerHTML = '';
+    screenBottomRow.innerHTML = '';
+}
+
+// FSM
+function buttonPressed() {
+    if (state == 0) {                           // cleared state
+        clearState();
+    } else if (state == 1) {                    // first operand state
+        screenBottomRow.innerHTML = firstOperand;
+        screenTopRow.innerHTML = firstOperand;
+    } else if (state == 2) {                    // operator state
+        operandHasDecimal = 0;
+        screenTopRow.innerHTML = firstOperand + operator;
+    } else if (state == 3) {                    // second operand state
+        screenTopRow.innerHTML = firstOperand + operator + secondOperand;
+        screenBottomRow.innerHTML = secondOperand;
+    } else if (state == 4) {
+        screenTopRow.innerHTML = firstOperand + operator + secondOperand + '=';
+        screenBottomRow.innerHTML = result;
+    } else if (state == 'Overflow') {
+        screenTopRow.innerHTML = '';
+        screenBottomRow.innerHTML = 'Overflow';
+    }
+
+    console.log(`state: ${state}, operand1: ${firstOperand}, operator: ${operator}, operand2: ${secondOperand}`);
+}
+
+function formatNumber(strNumber) {
+    if (strNumber.length > 9) {
+        if (strNumber % 1 != 0) {   // floating point number with decimal
+            while(strNumber.length > 9) { // chop off 1 decimal until it fits
+                strNumber = strNumber.slice(0, -1);
+            }
+        } else {                    // convert large numbers to exponential
+            strNumber = Number.parseFloat(strNumber).toExponential(2);
+        }
+
+    }
+    return strNumber;
+}
+
+
+buttonPressed();
