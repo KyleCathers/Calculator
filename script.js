c66016191d47d7719d5ac76b49efba51321d9db4
Kyle Cathers <kyle_c@live.ca>
2023-07-31 23:25:32 -0700

    Added plusminus functionality & chaining results
    Also updated HTML and JS to shorten repeated code for 0-9
    buttons using buttons array. Fixed backspace button functionality
    on large numbers (exponential).

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// state variables
 let firstOperand = '';
 let operator = '';
 let secondOperand = '';
@@ -5,74 +6,34 @@ let result = 0;
 let state = 0;
 let operandHasDecimal = 0;
 
+// screen
 let screenBottomRow = document.querySelector('.bottomRow');
 let screenTopRow = document.querySelector('.topRow');
 
-let oneButton = document.querySelector('#one');
-let twoButton = document.querySelector('#two');
-let threeButton = document.querySelector('#three');
-let fourButton = document.querySelector('#four');
-let fiveButton = document.querySelector('#five');
-let sixButton = document.querySelector('#six');
-let sevenButton = document.querySelector('#seven');
-let eightButton = document.querySelector('#eight');
-let nineButton = document.querySelector('#nine');
-let zeroButton = document.querySelector('#zero');
-let decimalButton = document.querySelector('#decimal');
+// 0-9 keys
+const buttons = [];
 
+for (let i = 0; i <= 9; i++) {
+    buttons[i] = document.querySelector("#button" + i);
+    buttons[i].addEventListener('click', () => { 
+        numberPressed(i.toString());
+    });
+}
+
+// other buttons
+let decimalButton = document.querySelector('#decimal');
 let addButton = document.querySelector('#add');
 let subtractButton = document.querySelector('#subtract');
 let multiplyButton = document.querySelector('#multiply');
 let divideButton = document.querySelector('#divide');
 let equalsButton = document.querySelector('#equals');
 let plusMinusButton = document.querySelector('#plusMinus');
-
 let clearButton = document.querySelector('#clear');
 let backspaceButton = document.querySelector('#backspace');
 
 clearButton.addEventListener('click', () => {
     state = 0;
     buttonPressed();
-});
-
-zeroButton.addEventListener('click', () => { 
-    numberPressed('0');
-});
-
-oneButton.addEventListener('click', () => {
-    numberPressed('1');
-});
-
-twoButton.addEventListener('click', () => {
-    numberPressed('2');
-});
-
-threeButton.addEventListener('click', () => {
-    numberPressed('3');
-});
-
-fourButton.addEventListener('click', () => {
-    numberPressed('4');
-});
-
-fiveButton.addEventListener('click', () => {
-    numberPressed('5');
-});
-
-sixButton.addEventListener('click', () => {
-    numberPressed('6');
-});
-
-sevenButton.addEventListener('click', () => {
-    numberPressed('7');
-});
-
-eightButton.addEventListener('click', () => {
-    numberPressed('8');
-});
-
-nineButton.addEventListener('click', () => {
-    numberPressed('9');
 });
 
 decimalButton.addEventListener('click', () => {
@@ -110,23 +71,17 @@ backspaceButton.addEventListener('click', () => {
         secondOperand = secondOperand.slice(0, -1);
         buttonPressed();
     } else if (state == 4) {
-        //console.log(`state: ${state}, operand1: ${firstOperand}, operator: ${operator}, operand2: ${secondOperand}`);
+        // only on non-exponential numbers
+        if(result.toString().indexOf("e+") == -1) {
+            result = result.slice(0, -1);
+            buttonPressed();
+        }
     }
 })
 
-function operate(a, b, operator) {
-    if (operator == '+') {
-        return a + b;
-    } else if (operator == '-') {
-        return a - b;
-    } else if (operator == 'x') {
-        return a * b;
-    } else if (operator == '/') {
-        return a/b;
-    } else {
-        return null;
-    }
-}
+plusMinusButton.addEventListener('click', () => {
+    plusMinusPressed();
+})
 
 function operatorPressed(operatorInput) {
     if (state == 1) {
@@ -137,8 +92,10 @@ function operatorPressed(operatorInput) {
         operator = operatorInput;
         buttonPressed();
     } else if (state == 4) {
-
-    } else if (state == 'Overflow') {
+        state = 2;
+        operator = operatorInput;
+        firstOperand = result;
+        secondOperand = '';
         buttonPressed();
     }
 }
@@ -173,19 +130,19 @@ function numberPressed(number) {
         }
         buttonPressed();
     } else if (state == 4) {
-        buttonPressed();
-    } else if (state == 'Overflow') {
-        buttonPressed();
+        buttonPressed(); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     }
 }
 
-function clearState() {
-    firstOperand = '';
-    operator = '';
-    secondOperand = '';
-    operandHasDecimal = 0;
-    screenTopRow.innerHTML = '';
-    screenBottomRow.innerHTML = '';
+function plusMinusPressed() {
+    if (state == 1) {
+        firstOperand = -firstOperand;
+    } else if (state == 3) {
+        secondOperand = -secondOperand;
+    } else if (state == 4) {
+        result = -result;
+    }
+    buttonPressed();
 }
 
 // FSM
@@ -197,6 +154,9 @@ function buttonPressed() {
         screenTopRow.innerHTML = firstOperand;
     } else if (state == 2) {                    // operator state
         operandHasDecimal = 0;
+        if (firstOperand == '') {
+            firstOperand = 0;
+        }
         screenTopRow.innerHTML = firstOperand + operator;
     } else if (state == 3) {                    // second operand state
         screenTopRow.innerHTML = firstOperand + operator + secondOperand;
@@ -204,12 +164,8 @@ function buttonPressed() {
     } else if (state == 4) {
         screenTopRow.innerHTML = firstOperand + operator + secondOperand + '=';
         screenBottomRow.innerHTML = result;
-    } else if (state == 'Overflow') {
-        screenTopRow.innerHTML = '';
-        screenBottomRow.innerHTML = 'Overflow';
     }
-
-    console.log(`state: ${state}, operand1: ${firstOperand}, operator: ${operator}, operand2: ${secondOperand}`);
+    console.log(`state: ${state}, operand1: ${firstOperand}, operator: ${operator}, operand2: ${secondOperand}, result = ${result}`);
 }
 
 function formatNumber(strNumber) {
@@ -226,5 +182,27 @@ function formatNumber(strNumber) {
     return strNumber;
 }
 
+function operate(a, b, operator) {
+    if (operator == '+') {
+        return a + b;
+    } else if (operator == '-') {
+        return a - b;
+    } else if (operator == 'x') {
+        return a * b;
+    } else if (operator == '/') {
+        return a/b;
+    } else {
+        return null;
+    }
+}
+
+function clearState() {
+    firstOperand = '';
+    operator = '';
+    secondOperand = '';
+    operandHasDecimal = 0;
+    screenTopRow.innerHTML = '';
+    screenBottomRow.innerHTML = '';
+}
 
 buttonPressed();
